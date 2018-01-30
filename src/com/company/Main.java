70e932b36fa49f06a49800d526819c3174025750
Karlas <karolis.zagarnauskas@gmail.com>
2018-01-30 14:57:33 +0200

    first commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,20 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        System.out.println("Sveiki, iveskite betkoki zodi");
+
+        Scanner sc = new Scanner(System.in);
+
+        String zodis = sc.nextLine();
+
+        System.out.println("zodis turi simboliu");
+
+        System.out.println(zodis.length());
+
     }
 }
